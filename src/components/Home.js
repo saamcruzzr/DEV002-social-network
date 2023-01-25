c99066689319c23522b2ed9ac5b7b9c4a692e166
Jess Inga Mayhua <jessinma40@gmail.com>
2023-01-25 03:35:13 -0500

    Implementación router para 3 vistas (bienvenida, iniciar sesión y registro)

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,24 +3,25 @@ import { onNavigate } from "../main.js";
 export const Home = () => {
     const HomeDiv = document.createElement('div');
     const msjInicio = `
-    <section>
-        <p>¡Bienvenidas y bienvenidos a SabiOld.
-        <br> Su red social favorita!
-        <br> Por favor, elige la opción que necesites</p>
+    <section class="section_home">
+        <p class="text_initial">¡Bienvenidas y bienvenidos a SabiOld. Su red social favorita!</p>
+        <p class="text_initial">Por favor, elige la opción que necesites</p>
     </section>
     `;
-    const btnRegister = document.createElement('button');
     const btnLogin = document.createElement('button');
+    const btnRegister = document.createElement('button');
+    btnLogin.setAttribute("class", "button btnLogin");
+    btnRegister.setAttribute("class", "button btnRegister");
 
     HomeDiv.innerHTML = msjInicio;
-    btnRegister.textContent = 'Registrarse';
     btnLogin.textContent = 'Iniciar Sesión';
+    btnRegister.textContent = 'Registrarse';
 
-    btnRegister.addEventListener('click', () => onNavigate('/register'));
     btnLogin.addEventListener('click', () => onNavigate('/login'));
+    btnRegister.addEventListener('click', () => onNavigate('/register'));
 
-    HomeDiv.appendChild(btnRegister);
     HomeDiv.appendChild(btnLogin);
-
+    HomeDiv.appendChild(btnRegister);
+    
     return HomeDiv;
 };
