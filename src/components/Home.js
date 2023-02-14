10a6f9a043261c0a76ea10f86bcbc2bc9c0d83d8
Jess Inga Mayhua <jessinma40@gmail.com>
2023-02-14 10:06:51 -0500

    Mejorar vista home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,32 +3,35 @@ import { onNavigate } from '../main.js';
 
 export const Home = () => {
   const HomeDiv = document.createElement('div');
-  const msjInicio = `
-    <header>
-      <div>
-        <h1 class="title">SabiOld</h1>
-      </div>
-    </header>
-    <img src='./IMG/4876586-01.png' alt='Adultos mayores usando tecnología'>
+  const sectionHome = `
     <section class='section_home'>
-        <p class='text_initial'>¡Bienvenidas y bienvenidos a SabiOld. Su red social favorita!</p>
-        <p class='text_initial'>Por favor, elige la opción que necesites</p>
+      <div class='img_home'>
+        <img src='./IMG/4876586-01.png' alt='Adultos mayores usando tecnología'>
+      </div>
+      <section class='welcome_home'>
+        <header class='title_home'>
+          <h1 class="title">SabiOld</h1>
+        </header>
+        <div class='txt_home'>
+            <p class='text_initial'>¡Bienvenidas y bienvenidos a SabiOld. Su red social favorita!</p>
+            <p class='text_initial'>Por favor, elige la opción que necesites</p>
+        </div>
+        <div class='txt_home'>
+          <button class='button btnLogin' id='btnLogin'>Iniciar Sesión</button>
+          <button class='button btnRegister' id='btnRegister'>Registrarse</button>
+        </div>
+      </section>
     </section>
     `;
-  const btnLogin = document.createElement('button');
-  const btnRegister = document.createElement('button');
-  btnLogin.setAttribute('class', 'button btnLogin');
-  btnRegister.setAttribute('class', 'button btnRegister');
 
-  HomeDiv.innerHTML = msjInicio;
-  btnLogin.textContent = 'Iniciar Sesión';
-  btnRegister.textContent = 'Registrarse';
-
-  btnLogin.addEventListener('click', () => onNavigate('/login'));
-  btnRegister.addEventListener('click', () => onNavigate('/register'));
-
-  HomeDiv.appendChild(btnLogin);
-  HomeDiv.appendChild(btnRegister);
+  HomeDiv.innerHTML = sectionHome;
 
   return HomeDiv;
 };
+
+window.addEventListener('DOMContentLoaded', () => {
+  const btnLogin = document.getElementById('btnLogin');
+  const btnRegister = document.getElementById('btnRegister');
+  btnLogin.addEventListener('click', () => onNavigate('/login'));
+  btnRegister.addEventListener('click', () => onNavigate('/register'));
+});
