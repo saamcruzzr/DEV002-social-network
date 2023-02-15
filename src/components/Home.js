52a537e80837aec7a7a17211b9b07e8eef800696
FerRomero1990 <aidafernanda1990@hotmail.com>
2023-02-15 01:12:25 -0500

    Trabajando CSS para feed 15 feb

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
         <h1 class="title">SabiOld</h1>
       </div>
     </header>
-    <img src='./IMG/4876586-01.png' alt='Adultos mayores usando tecnología'>
+    <img src='./IMG/4876586-01.png' alt='Adultos mayores usando tecnología' class='img_adul_may'>
     <section class='section_home'>
         <p class='text_initial'>¡Bienvenidas y bienvenidos a SabiOld. Su red social favorita!</p>
         <p class='text_initial'>Por favor, elige la opción que necesites</p>
