dc4b8decc9ab0044180ecae073fc791a5b2d4863
saamcruzzr <116410945+saamcruzzr@users.noreply.github.com>
2023-02-28 09:53:35 -0600

    Delete fconfig.js

--- src/firebase/fconfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDzJQ-6aUeu7ckM2B4I3wSHh6eKgc6q5Hg',
-  authDomain: 'sn-sabiold.firebaseapp.com',
-  projectId: 'sn-sabiold',
-  storageBucket: 'sn-sabiold.appspot.com',
-  messagingSenderId: '104307031324',
-  appId: '1:104307031324:web:1114a8ca831f2ac8c54ed9',
-};
